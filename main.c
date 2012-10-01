1a4b283157e3f7086addc50c6f55edbd04cb446a
Tom Lee <github@tomlee.co>
2012-09-30 18:44:14 -0700

    Call chimp_core_startup/shutdown in main.

--- main.c
+++ main.c
@@ -3,17 +3,11 @@
 int
 main (int argc, char **argv)
 {
-    ChimpRef *ref;
-    ChimpGC *gc = chimp_gc_new ();
+    if (!chimp_core_startup ()) {
+        return 1;
+    }
 
-    ref = chimp_gc_new_object (gc);
-    CHIMP_ANY(ref)->type = CHIMP_VALUE_TYPE_OBJECT;
-    CHIMP_ANY(ref)->klass = NULL;
-    chimp_gc_make_root (gc, ref);
-
-    chimp_gc_collect (gc);
-
-    chimp_gc_delete (gc);
+    chimp_core_shutdown ();
     return 0;
 }
 
