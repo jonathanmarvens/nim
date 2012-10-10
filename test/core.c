88b010399c08d0902e4ec2706036d52c9a3d633b
Tom Lee <github@tomlee.co>
2012-10-10 01:28:34 -0700

    Added our first unit test.

---
+++ test/core.c
@@ -0,0 +1,13 @@
+#include <check.h>
+
+#include <chimp.h>
+
+START_TEST (test_startup_shutdown)
+{
+    int stack;
+
+    fail_unless (chimp_core_startup ((void *)&stack), "expected startup to succeed");
+    chimp_core_shutdown ();
+}
+END_TEST
+
