943b982177a36cdcdd73f6733279064d85898861
Tom Lee <github@tomlee.co>
2012-10-10 03:49:05 -0700

    Add the 'str' object to our locals hash for kicks.

--- main.c
+++ main.c
@@ -90,6 +90,7 @@ real_main (int argc, char **argv)
         chimp_hash_put_str (locals, "print", chimp_method_new_native (NULL, _print));
         chimp_hash_put_str (locals, "hash", chimp_hash_class);
         chimp_hash_put_str (locals, "array", chimp_array_class);
+        chimp_hash_put_str (locals, "str", chimp_str_class);
         result = chimp_vm_eval (NULL, code, locals);
         if (result == NULL) {
             fprintf (stderr, "error: chimp_vm_eval () returned NULL\n");
