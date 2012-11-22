6032e5d57f9b814900ef6f2a5d6439e817db29dc
Tom Lee <github@tomlee.co>
2012-11-21 22:16:50 -0800

    make distcheck fixes + update tests

--- test/test_str.c
+++ test/test_str.c
@@ -12,7 +12,7 @@ test_str_teardown (void)
 
 START_TEST(str_constructor_with_no_args_returns_empty_string)
 {
-    ChimpRef *obj = chimp_object_call (chimp_str_class, chimp_array_new (NULL));
+    ChimpRef *obj = chimp_object_call (chimp_str_class, chimp_array_new ());
     fail_unless (strcmp("", CHIMP_STR_DATA(obj)) == 0, "expected empty string");
     fail_unless (CHIMP_STR_SIZE(obj) == 0, "empty strings should have a size of zero");
 }
@@ -20,7 +20,7 @@ END_TEST
 
 START_TEST(str_constructor_with_str_arg_returns_str_value)
 {
-    ChimpRef *obj = chimp_object_call (chimp_str_class, chimp_array_new_var (NULL, CHIMP_STR_NEW (NULL, "testing"), NULL));
+    ChimpRef *obj = chimp_object_call (chimp_str_class, chimp_array_new_var (CHIMP_STR_NEW ("testing"), NULL));
     fail_unless (strcmp ("testing", CHIMP_STR_DATA(obj)) == 0,
                 "expected a populated string");
 }
@@ -28,7 +28,7 @@ END_TEST
 
 START_TEST(str_constructor_with_int_arg_returns_int_value)
 {
-    ChimpRef *obj = chimp_object_call (chimp_str_class, chimp_array_new_var (NULL, chimp_int_new (NULL, 4096), NULL));
+    ChimpRef *obj = chimp_object_call (chimp_str_class, chimp_array_new_var (chimp_int_new (4096), NULL));
     fail_unless (strcmp ("4096", CHIMP_STR_DATA(obj)) == 0,
                 "expected the int to be converted to a string");
 }
