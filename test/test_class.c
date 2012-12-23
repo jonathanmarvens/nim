c0a1c433f19330c8cf01d4401ffd6dd7bbbcf50d
Tom Lee <github@tomlee.co>
2012-12-22 20:00:21 -0800

    Apache 2 license.

--- test/test_class.c
+++ test/test_class.c
@@ -1,3 +1,21 @@
+/*****************************************************************************
+ *                                                                           *
+ * Copyright 2012 Thomas Lee                                                 *
+ *                                                                           *
+ * Licensed under the Apache License, Version 2.0 (the "License");           *
+ * you may not use this file except in compliance with the License.          *
+ * You may obtain a copy of the License at                                   *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ *                                                                           *
+ *****************************************************************************/
+
 #define CHIMP_TEST_INSTANCE_CHECK(inst, klass, gc_type) \
     fail_unless ((inst) != NULL, "calling %s constructor failed", CHIMP_STR_DATA(CHIMP_CLASS_NAME(klass))); \
     fail_unless (CHIMP_ANY_TYPE((inst)) == (gc_type), "object should be GC type #%d", (gc_type)); \
